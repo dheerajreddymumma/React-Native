3317aed8013a84af9a69a96d9ec16e39c0f1c2e6
= <=>
2019-06-02 13:52:34 +0530

    basic setup

--- my-app/components/MenuComponent.js
+++ my-app/components/MenuComponent.js
@@ -12,6 +12,7 @@ function Menu(props) {
                     title={item.name}
                     subtitle={item.description}
                     hideChevron={true}
+                    onPress={() => props.onPress(item.id)}
                     leftAvatar={{ source: require('./images/uthappizza.png')}}
                   />
         );
